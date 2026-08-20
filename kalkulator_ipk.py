8a1de055802de29f388dfe0ed52504bee5b5870f
Arridho Ramadhan Firdaus <sinsarridho@gmail.com>
2026-08-20 15:35:15 +0700

    sesi2

---
+++ kalkulator_ipk.py
@@ -0,0 +1,9 @@
+sks_matematika = 3
+sks_bahasa = 2
+
+total_sks = sks_matematika + sks_bahasa
+total_nilai = 18.5 # Anggap saja ini total perkalian nilainya
+
+ipk = total_nilai / total_sks
+print("Total SKS saya adalah: " + str(total_sks))
+print("IPK saya semester ini: " + str(ipk))
---
+++ nilai_mhs.py
@@ -0,0 +1,4 @@
+nama = "Athaya"
+nilai_algoritma = 90
+print("Nilai " + nama + " adalah: " + str(nilai_algoritma))
+      
