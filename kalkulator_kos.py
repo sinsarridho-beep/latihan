5de177bc23e1415686d18544c123f58e8b944839
Arridho Ramadhan Firdaus <sinsarridho@gmail.com>
2026-09-01 21:05:37 +0700

    sesi 3

---
+++ kalkulator_kos.py
@@ -0,0 +1,10 @@
+print("= Kalkulator Anak Kos =")
+
+# 1. Tambahkan int(input(...)) di bawah ini
+uang_saku = int(input("Masukkan uang saku bulanan: "))
+biaya_kos = int(input("Masukkan biaya kos per bulan: "))
+
+# 2. Kurangi uang saku dengan biaya kos
+sisa = uang_saku - biaya_kos
+
+print("Sisa uang jajanmu bulan ini: Rp " + str(sisa))
